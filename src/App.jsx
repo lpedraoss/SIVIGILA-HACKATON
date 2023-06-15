f41a05a219838b7fa74e1858bef241b42ca1a945
lpedraoss <alfonso.suarez.dev@gmail.com>
2023-06-15 16:16:10 -0500

    modified:     - se modifico la posicion del grafico

--- src/App.jsx
+++ src/App.jsx
@@ -1,41 +1,73 @@
-import { useEffect, useState } from 'react';
+import  { useEffect, useState } from 'react';
 import './App.css';
 import UseDiccionario from './data/diccionarios/rutina';
 import Header from './components/pure/header';
 import BarChartComponent from './components/pure/grafico';
+import { EVENTO } from './data/utils/evento.enum';
+import { DEPARTAMENTO } from './data/utils/departamento.enum';
 
-function App() {
+const App = () => {
   const { rutina } = UseDiccionario();
   const [datosFiltrados, setDatosFiltrados] = useState([]);
-  const [mostrarGrafico, setMostrarGrafico] = useState(false);
 
+  const [typeEvent, setTypeEvent] = useState(EVENTO.DENGUE); // Estado para almacenar el typeEvent seleccionado
+  const [typeDepto, setTypeDepto] = useState(DEPARTAMENTO.BOGOTA)
   useEffect(() => {
-    const filtrarDatos = rutina.filter((dato) => dato.codDptoO === 0);
-    setDatosFiltrados(filtrarDatos);
-  }, [rutina]);
+    const filtrarDatos = rutina.filter((dato) => {
+      let event = dato.codEve === typeEvent;
+      let filtro = event;
 
+      return filtro;
+    }
+    
+
+    );
+    setDatosFiltrados(filtrarDatos);
+  }, [rutina, typeEvent]);
+
+  const handleTypeEventChange = (event) => {
+    setTypeEvent(Number(event.target.value));
+  };
+  const handleTypeDeptoChange = (depto) => {
+    setTypeDepto(Number(depto.target.value));
+  }
   return (
     <>
       <Header />
       <main>
+      <BarChartComponent data={datosFiltrados} />
         <article id="contenedor_producto">
           <ul id="productos">
             {datosFiltrados.map((dato) => (
               <li key={dato.id}>
-                <h2>{"nombre de evento: " + dato.ndepProce}</h2>
                 <h2>{"nombre de evento: " + dato.nomEve}</h2>
-                <h2>{"grandTotal: " + dato.grandTotal}</h2>
+                <h2>{"casos totales: " + dato.grandTotal}</h2>
               </li>
             ))}
           </ul>
+          <> 
+             
+          <div  >
+             <label  htmlFor="typeEvent">Selecciona un evento:</label>
+             <select id="typeEvent" value={typeEvent} onChange={handleTypeEventChange}>
+               <option value={EVENTO.DENGUE}>Dengue</option>
+               <option value={EVENTO.HEPATITIS_A}>Hepatitis A</option>
+             </select>
+             <label htmlFor="typeEvent">Selecciona un departamento:</label>
+             <select id="typeDepto" value={typeDepto} onChange={handleTypeDeptoChange}>
+               <option value={DEPARTAMENTO.BOGOTA}>Bogota</option>
+               <option value={DEPARTAMENTO.ANTIOQUIA}>Antioquia</option>
+               <option value={DEPARTAMENTO.ATLANTICO}>Antlantico</option>
+             </select>
+           </div> 
+         </>
         </article>
-        <button onClick={() => setMostrarGrafico(!mostrarGrafico)}>
-          Mostrar/ocultar gráfico
-        </button>
-        {mostrarGrafico && <BarChartComponent data={datosFiltrados} />}
+
+         
+
       </main>
     </>
   );
-}
+};
 
 export default App;
