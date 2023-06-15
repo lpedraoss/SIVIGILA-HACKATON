4a9d5fa78045331b7f440219df1d86f79f4faf1e
lpedraoss <alfonso.suarez.dev@gmail.com>
2023-06-15 12:22:05 -0500

    added:     - filtros en el api

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,38 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+/* eslint-disable no-unused-vars */
+/* eslint-disable no-undef */
+
+import { useEffect, useState } from 'react';
 import './App.css'
+import UseDiccionario from './data/diccionarios/rutina'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const { rutina } = UseDiccionario();
+  const [state, setstate] = useState([]);
+
+  useEffect(() => {
+    setstate(rutina);
+  }, [rutina]);
+
+  // Filtrar los datos por ciertas cantidades
+  const datosFiltrados = rutina.filter((dato) => {
+    let codeDpto = dato.codDptoO === 5
+    let id = dato.id  === 67
+    return  codeDpto;
+  });
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <article id="contenedor_producto">
+        <ul id="productos">
+          {datosFiltrados.map((dato) => (
+            <li key={dato.id}>
+              <h2>{"nombre de evento: " + dato.ndepProce}</h2>
+              <h2>{"nombre de evento: " + dato.nomEve}</h2>
+              <h2>{"grandTotal: " + dato.grandTotal}</h2>
+            </li>
+          ))}
+        </ul>
+      </article>
     </>
   )
 }
